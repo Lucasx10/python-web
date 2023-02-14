5a9046260bc78d018dc2f4c2742193dc18ec2090
Lucas Prado <lukasprado260@gmail.com>
2023-02-14 23:42:09 +0000

    inciando projeto

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# POSTS MOCK
+posts = [
+    {
+        "titulo": "Post 1",
+        "texto": "Meu primeiro Post"
+    }, 
+    {
+        "titulo": "Post 2",
+        "texto": "Meu segundo Post"
+    }, 
+    {
+        "titulo": "Post 3",
+        "texto": "Novo Post"
+    }
+]
+
+@app.route('/')
+def exibir_entradas():
+    return render_template("exibir_entradas.html", entradas=posts)
